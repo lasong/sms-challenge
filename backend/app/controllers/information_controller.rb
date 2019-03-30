55ff1ad6143cdcfee5393f622107b78a01e9b1fd
Lekealem Asong <lekealemasong@yahoo.de>
2019-03-30 10:46:54 +0100

    Finish adding CRUD endpoints

--- backend/app/controllers/information_controller.rb
+++ backend/app/controllers/information_controller.rb
@@ -1,9 +1,52 @@
 class InformationController < ApplicationController
+  before_action :set_information, only: [:show, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+
   def index
-    render json: query_information.map { |info| serialize(info) }
+    render json: query_information.map { |information| serialize(information) }
+  end
+
+  def show
+    render json: serialize(@information)
+  end
+
+  def create
+    info = Information.create(information_params)
+
+    if info.invalid?
+      render json: { errors: info.errors }, status: :unprocessable_entity and return
+    end
+
+    render json: serialize(info), status: :created
+  end
+
+  def update
+    @information.update(information_params)
+
+    if @information.invalid?
+      errors = @information.errors
+      render json: { errors: errors }, status: :unprocessable_entity and return
+    end
+
+    render json: serialize(@information)
+  end
+
+  def destroy
+    @information.destroy
+
+    render json: { message: 'Information successfully deleted' }
   end
 
   private
+
+  def set_information
+    @information = Information.find(params[:id])
+  end
+
+  def information_params
+    params.require(:information).permit(
+      :city, :start_date, :end_date, :price, :status, :color)
+  end
 
   def query_information
     if params[:start_date].present? && params[:end_date].present?
@@ -12,6 +55,10 @@ class InformationController < ApplicationController
     end
 
     Information.all
+  end
+
+  def handle_record_not_found
+    render json: { error: 'Information not found' }, status: :not_found
   end
 
   def serialize(information)
--- backend/spec/requests/information_spec.rb
+++ backend/spec/requests/information_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Consultations', type: :request do
+RSpec.describe 'Information', type: :request do
   let(:attributes) do
     {
       city: 'Buea',
@@ -68,6 +68,103 @@ RSpec.describe 'Consultations', type: :request do
         expect(response).to be_success
         expect(json_response.length).to eq 2
       end
+    end
+  end
+
+  describe 'GET /information/:id' do
+    it 'returns information for given id' do
+      id = Information.first.id
+
+      get "/information/#{id}"
+
+      expect(response).to be_success
+      expect(json_response['city']).to eq 'Buea'
+      expect(json_response['start_date']).to eq '2019-03-29'
+      expect(json_response['end_date']).to eq '2019-05-29'
+      expect(json_response['price']).to eq 45.64
+      expect(json_response['status']).to eq 'Weekly'
+      expect(json_response['color']).to eq '#800080'
+    end
+
+    it 'returns 404 response status if information does not exist' do
+      id = Information.first.id
+
+      get "/information/#{id + 42}"
+
+      expect(response).to have_http_status(:missing)
+    end
+  end
+
+  describe 'POST /information' do
+    it 'creates information and returns created information' do
+      params = { information: attributes.merge(city: 'Tiko', price: 6.8) }
+      post '/information', params: params
+
+      expect(response).to have_http_status(:created)
+      expect(json_response['id']).to_not be_nil
+      expect(json_response['city']).to eq 'Tiko'
+      expect(json_response['price']).to eq 6.8
+    end
+
+    context 'errors' do
+      it 'does not create information if attributes are not valid' do
+        params = { information: attributes.merge(status: nil) }
+
+        expect { post '/information', params: params }.to_not change(Information, :count)
+      end
+
+      it 'returns unprocessable entity status code if attributes are not valid' do
+        params = { information: attributes.merge(color: 'color') }
+        post '/information', params: params
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe 'PUT /information/:id' do
+    before { @id = Information.first.id }
+
+    it 'updates information and returns updated information' do
+      put "/information/#{@id}", params: { information: { city: 'Fontem' } }
+
+      expect(response).to be_success
+      expect(json_response['city']).to eq 'Fontem'
+    end
+
+    context 'errors' do
+      it 'does not change information if attributes are not valid' do
+        put "/information/#{@id}", params: { information: { city: nil } }
+
+        expect(Information.first.city).to eq 'Buea'
+      end
+
+      it 'returns unprocessable entity status code if attributes are not valid' do
+        put "/information/#{@id}", params: { information: { end_date: nil } }
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe 'DELETE /information/:id' do
+    before { @id = Information.first.id }
+
+    it 'deletes information for given id' do
+      expect{ delete "/information/#{@id}" }.to change(Information, :count).by(-1)
+    end
+
+    it 'returns delete success message' do
+      delete "/information/#{@id}"
+
+      expect(response).to be_success
+      expect(json_response['message']).to eq 'Information successfully deleted'
+    end
+
+    it 'returns 404 response status if information does not exist' do
+      delete "/information/#{@id + 42}"
+
+      expect(response).to have_http_status(:missing)
     end
   end
 end
